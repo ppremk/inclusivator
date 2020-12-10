ffd0e47f8695653a87374cb3f3a753ab552413f8
Stefan Stölzle <stoe@github.com>
2020-12-10 16:42:53 +0100

    ‼ Refactor
    Co-authored-by: Bassem Dghaidi <link-@github.com>
    Co-authored-by: Mala Kumar <malakumar85@github.com>
    Co-authored-by: Prem Kumar Ponuthorai <ppremk@github.com>

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Required, set's the non-inclusivity threshold in bytes. Default ".5".
 
 #### `token `
 
-Optional. Takes a valid **GitHub Token** from the Repo by default. 
+Optional. Takes a valid **GitHub Token** from the Repo by default.
 
 ## Outputs
 
@@ -26,5 +26,7 @@ Consume the action by referencing the stable branch
 uses: actionsdesk/inclusivator@v1.0
 with:
   token: ${{ secrets.GITHUB_TOKEN }} # Optional
-  tba: '' 
+  tba: '0.5'
+env:
+  PERSPECTIVE_API_KEY: ${{ secrets.PERSPECTIVE_API_KEY }}
 ```
--- index.js
+++ index.js
@@ -1,103 +1,93 @@
-const core = require("@actions/core")
-const github = require("@actions/github")
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-const Perspective = require("perspective-api-client")
+const Perspective = require('perspective-api-client')
 
-const myToken = core.getInput("token")
-const octokit = new github.GitHub(myToken)
-
-const context = github.context
-
-const { owner, repo } = context.repo
+const {context} = github
+const {owner, repo} = context.repo
 const event_type = context.eventName
 
-let issue_pr_number
-const labels = ["non-inclusivity-detected!"]
+const labels = ['non-inclusivity-detected!']
 
-// most @actions toolkit packages have async methods
-async function run() {
+;(async () => {
   try {
-    const tba = core.getInput("tba")
+    const tba = core.getInput('tba', {required: true})
+
+    const myToken = core.getInput('token')
+    const octokit = new github.GitHub(myToken)
 
     const perspective = new Perspective({
-      apiKey: process.env.PERSPECTIVE_API_KEY,
+      apiKey: process.env.PERSPECTIVE_API_KEY
     })
 
-    core.info(`Default configured reply comment is set to ${tba} ...`)
-    core.info(`Name of Repository is ${repo} and the owner is ${owner}`)
-    core.info(`Triggered event is ${event_type}`)
+    core.debug(`Default configured reply comment is set to ${tba} ...`)
+    core.debug(`Name of Repository is ${repo} and the owner is ${owner}`)
+    core.debug(`Triggered event is ${event_type}`)
 
-    // Get Non Inclusivity Warning Label
-    let nonInclusivelabelObj = {}
     try {
-      nonInclusivelabelObj = await octokit.issues.getLabel({
+      await octokit.issues.getLabel({
         owner,
         repo,
-        name: "non-inclusivity-detected!",
+        name: 'non-inclusivity-detected!'
       })
-    } catch (error) {
-      if (error.message === "Not Found") {
+    } catch (err) {
+      if (err.message === 'Not Found') {
         await octokit.issues.createLabel({
           owner,
           repo,
-          name: "non-inclusivity-detected!",
-          color: "ff1493",
-          description:
-            "Warning Label for use when non inclusive language is detected in the commits of a Issue or Pull Request",
+          name: 'non-inclusivity-detected!',
+          color: 'ff1493',
+          description: 'Non inclusive language is detected in the commits of a Issue or Pull Request'
         })
-        core.info(`No non-inclusivity label detected. Creating new label ...`)
-        core.info(`Warning label created`)
+
+        core.debug(`No non-inclusivity label detected. Creating new label ...`)
+        core.debug(`Warning label created`)
       } else {
-        core.error(`getLabel error: ${error.message}`)
+        core.error(`getLabel error: ${err.message}`)
       }
     }
 
-    // Get Issue or Pull Request
-    if (event_type === "pull_request") {
-      issue_pr_number = context.payload.pull_request.number
+    let issue_number
 
-      core.info(`The PR number is: ${issue_pr_number}`)
+    // Get Issue or Pull Request
+    if (event_type === 'pull_request') {
+      issue_number = context.payload.pull_request.number
     }
 
     // Get Issue or Pull Request
-    if (event_type === "issues") {
-      issue_pr_number = context.payload.issues.number
-
-      core.info(`The Issue number is: ${issue_pr_number}`)
+    if (event_type === 'issues') {
+      issue_number = context.payload.issues.number
     }
+
+    core.debug(`The ${event_type} number is: ${issue_number}`)
 
     // TODO Logic for detecting spam/non inclusive language
-    const body = context.payload.comment.body
-    const response = await perspective.analyze(body, { truncate: true })
+    const {body} = context.payload.comment
+    const {attributeScores} = await perspective.analyze(body, {truncate: true})
 
-    const inclusivityPoint =
-      response.attributeScores.TOXICITY.summaryScore.value
+    const {value} = attributeScores.TOXICITY.summaryScore
 
-    if (inclusivityPoint >= tba) {
-      let bodyTemplate = `## :warning: Possible non inclusive language detected :warning: \n Dude! cmon.. u can do better.. check the Code of Conduct pls!`
-
+    if (value >= tba) {
       await octokit.issues.addLabels({
         owner,
         repo,
-        issue_number: issue_pr_number,
-        labels,
+        issue_number,
+        labels
       })
 
       await octokit.issues.createComment({
         owner,
         repo,
-        issue_number: issue_pr_number,
-        body: bodyTemplate,
+        issue_number,
+        body: `## :warning: Possible non inclusive language detected :warning:
+
+Dude! cmon.. u can do better.. check the Code of Conduct pls!`
       })
 
-      core.setFailed(
-        `Non inclusivity detected! Setting Issue or PR status to flaged.`
-      )
+      core.setOutput(`Non inclusivity detected! Setting Issue or PR status to flaged.`)
     }
     // LOGIC ENDS HERE
-  } catch (error) {
-    core.setFailed(error.message)
+  } catch (err) {
+    core.setFailed(err.message)
   }
-}
-
-run()
+})()
